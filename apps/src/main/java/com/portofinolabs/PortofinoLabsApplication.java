baa620ee26ea63890442d14caec9163fdb51155f
Matheus Fechine de Moura <matheus.moura@fortaleza.ce.gov.br>
2018-04-26 17:00:50 -0300

    Adding Spring Redis framework. Refactoring packages.

--- apps/src/main/java/com/portofinolabs/PortofinoLabsApplication.java
+++ apps/src/main/java/com/portofinolabs/PortofinoLabsApplication.java
@@ -1,4 +1,4 @@
-package com.portofinolabs.PortofinoLabs;
+package com.portofinolabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ apps/src/main/java/com/portofinolabs/model/LogDetail.java
@@ -0,0 +1,7 @@
+package com.portofinolabs.model;
+
+public class LogDetail {
+
+	//{"logset": ["endpoint": "hello-world", "logs": [{"ip":"xxx.xxx.xxx.xxx", "timestamp": xxxxxxxxxxx}]]}
+	
+}
